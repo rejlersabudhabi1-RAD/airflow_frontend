c7f68d7082f4f836d1fb68d3cf961e91b988683f
Abdullah omar khan <abdullah.khan@rejlers.ae>
2026-02-05 11:09:56 +0400

    feat: Add PFD Verification page with 8 reference documents (7 required, 1 optional)

--- src/App.jsx
+++ src/App.jsx
@@ -66,6 +66,7 @@ import NotFound from './pages/NotFound'
 import DesignIQDashboard from './pages/DesignIQ/DesignIQDashboard'
 import DesignIQLists from './pages/DesignIQ/DesignIQLists'
 import StressCriticalLineList from './pages/DesignIQ/StressCriticalLineList'
+import PFDVerification from './pages/DesignIQ/PFDVerification'
 import DesignIQProjectDetail from './pages/DesignIQ/DesignIQProjectDetail'
 import DesignIQNewProject from './pages/DesignIQ/DesignIQNewProject'
 // Procurement Components
@@ -538,6 +539,14 @@ function App() {
           }
         />
         <Route
+          path="designiq/pfd-verification"
+          element={
+            <ModuleProtectedRoute moduleCode="designiq">
+              <PFDVerification />
+            </ModuleProtectedRoute>
+          }
+        />
+        <Route
           path="designiq/projects/:id"
           element={
             <ModuleProtectedRoute moduleCode="designiq">
@@ -722,4 +731,6 @@ function App() {
 }
 
 export default App
+
+
 
