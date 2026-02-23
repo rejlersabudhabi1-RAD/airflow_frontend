8257d7c64dfa19709d16e49cf34782219183ec13
Rejlers Abu Dhabi <rejlersabudhabi1-RAD@users.noreply.github.com>
2026-02-23 12:08:12 +0400

    feat: add ProcessEquipmentHub with P&ID upload functionality for MOV/SDV datasheets

--- src/App.jsx
+++ src/App.jsx
@@ -83,6 +83,7 @@ import ProcessDatasheetPage from './pages/ProcessDatasheetPage'
 import ComprehensivePumpForm from './pages/ProcessDatasheet/ComprehensivePumpForm'
 import PumpDataSheetView from './pages/ProcessDatasheet/PumpDataSheetView'
 import PressureInstrumentPage from './pages/ProcessDatasheet/PressureInstrumentPage'
+import ProcessEquipmentHub from './pages/Engineering/Process/ProcessEquipmentHub'
 // Electrical Datasheet Components
 import ElectricalDocumentsHub from './pages/Engineering/Electrical/ElectricalDocumentsHub'
 import ElectricalDatasheetPage from './pages/Engineering/Electrical/ElectricalDatasheetPage'
@@ -663,6 +664,16 @@ function App() {
           element={
             <ProtectedRoute>
               <PressureInstrumentPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Process Equipment Hub - Unified Document Interface with P&ID Upload */}
+        <Route
+          path="engineering/process/datasheet/equipment"
+          element={
+            <ProtectedRoute>
+              <ProcessEquipmentHub />
             </ProtectedRoute>
           }
         />
